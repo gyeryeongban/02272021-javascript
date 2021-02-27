c9249c3cc08b6825695e997515f7dd78b9df29cb
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-02-28 07:54:05 +0900

    03 JavaScript Data Type

--- javascrpt 03/variable.js
+++ javascrpt 03/variable.js
@@ -3,7 +3,7 @@
 // use this for valina Javavscript
 // 'use strict';
 
-// 2. Variable
+// 2. Variable, rw(read/write)
 // let (added in ES6)
 
 let globaName = 'global name';
@@ -25,13 +25,19 @@ var age;
 }
 console.log(age);
 
-// 3. Constants
+// 3. Constant, r(read only)
+// use const whenever possible
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type alaways for a few reasons:
 // - Security
 // - thread safety
 // - reduce human mistakes
-const daysInWeek = 7;
-const maxNumber = 5;
 
 // 4. Variable types
 // primitive, single item: Number, string, boolean, null, undefiedn, Symbol
